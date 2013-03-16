2649e3598f2567d0847722fdcfa4da07c1bfde7c
jon grant <jon@alfieandjon.com>
2013-03-16 16:43:14 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jghotdesk::Application.config.secret_token = 'f36e56208b59337b5dd9d51705fd40518ceaf177c2069430af58a8ca8ebe479ab7d92c97aa05f5ec7ccb2043ef9ed71352f853bebae548f1d135c610895828a4'
